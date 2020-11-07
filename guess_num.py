a2e8887aa92b8eb24d31bb513d032529b84c35d3
DSKrypton <36557772+DSKrypton@users.noreply.github.com>
2020-11-07 06:53:00 +0200

    task(guess_num): completed

--- guess_num.py
+++ guess_num.py
@@ -13,6 +13,35 @@
 
 Тебе может понадобится модуль random, цикл while и ветвления
 """
+from random import randint
+MIN_VALUE = 0
+MAX_VALUE = 1000000
+
+
+def manual_binary_search_trainer():
+    value_to_guess = randint(MIN_VALUE, MAX_VALUE)
+    print(f'Greetings, The Number from {MIN_VALUE} to {MAX_VALUE} range has been chosen, type your guesses.')
+    next_line = input('Next guess:')
+    while next_line not in ('stop', 'end', 'exit'):
+        try:
+            next_value = int(next_line)
+        except ValueError:
+            print(f'Something went wrong.{next_line} is not a number')
+            continue
+        if next_value in range(MIN_VALUE, MAX_VALUE):
+            if next_value > value_to_guess:
+                print(f'{next_value} is greater than The Number')
+            elif next_value < value_to_guess:
+                print(f'{next_value} is lesser than The Number')
+            else:
+                print('Congratulations! You guessed The Number')
+                exit(0)
+        else:
+            print(f'{next_value} is outside of guessing range. It\'s between {MIN_VALUE} and {MAX_VALUE}')
+        next_line = input('Next guess:')
+
+
+
 
 if __name__ == '__main__':
-    pass
+    manual_binary_search_trainer()
