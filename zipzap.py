e81438108d8ad033386f2888e320e5aaabcc6419
DSKrypton <36557772+DSKrypton@users.noreply.github.com>
2020-11-07 07:15:11 +0200

    task(zipzap): completed + zup extra

--- zipzap.py
+++ zipzap.py
@@ -8,5 +8,26 @@
 Тебе может понадобиться цикл for и ветвления
 """
 
+
+def zipzapzup(x):
+    [print(f'{i}: zip-zap-zup') if i % 105 == 0 else
+     print(f'{i}: zap-zup') if i % 35 == 0 else
+     print(f'{i}: zip-zup') if i % 21 == 0 else
+     print(f'{i}: zip-zap') if i % 15 == 0 else
+     print(f'{i}: zup') if i % 7 == 0 else
+     print(f'{i}: zap') if i % 5 == 0 else
+     print(f'{i}: zip') if i % 3 == 0 else
+     print(i) for i in range(1, x+1)
+     ]
+
+
 if __name__ == '__main__':
-    pass
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print('zip-zap')
+        elif i % 5 == 0:
+            print('zap')
+        elif i % 3 == 0:
+            print('zip')
+        else:
+            print(i)
